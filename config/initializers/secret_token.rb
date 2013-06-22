01f6f9e50dc9d5c8b77831e0f6e687b6bdb9f719
Yann Klis <yann.klis@novelys.com>
2013-06-22 14:36:42 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watchtonight::Application.config.secret_key_base = 'c34a12090ede580f7e4ebe808194bf7e3792988b3f5b1cef6401c3472cc35003053b65b9889876b42c6e4221a8972b61c8297940fb0744b12586337a269445f2'
